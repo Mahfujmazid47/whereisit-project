2b13a732b39a1c1e65bf8afb9f0d56e58c6325bb
Mahfujmazid47 <mahfujmazid47@gmail.com>
2025-06-16 20:37:37 +0600

    added framer motion in home page

--- src/Pages/Home/Slider.jsx
+++ src/Pages/Home/Slider.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
+import { motion } from "motion/react"
 
 const Slider = () => {
     return (
         <div className="carousel w-full max-h-screen mb-24">
 
-            <div id="slide1" className="carousel-item relative w-full">
-                <div className='lg:p-26 md:p-14 py-8 space-y-5 md:space-y-0 md:flex justify-center items-center rounded-b-xl bg-purple-300'>
-                    <div>
+            <div id="slide1" className="carousel-item relative w-full overflow-hidden">
+                <div className='lg:p-26 md:p-14 py-8 space-y-0 md:space-y-0 md:flex justify-center items-center rounded-b-xl bg-purple-300'>
+                    <div className='overflow-hidden'>
                         <Fade direction='left' triggerOnce={true}>
-                            <h1 className='text-center md:text-left text-3xl md:text-4xl font-bold lg:text-5xl mb-2'><span className='text-purple-500'>Where</span>IsIt</h1>
-                        <p className='text-base-400 text-sm text-center md:text-left md:text-base mb-8 md:mb-0'>Reunite lost treasures and found items effortlessly – your community's dedicated lost and found platform for quick connections.</p>
+                            <h1 className='text-center md:text-left text-3xl md:text-4xl font-bold lg:text-5xl lg:w-3/4 mb-2'><span className='text-purple-500'>Where</span>IsIt</h1>
+                            <p className='text-base-400 text-sm text-center md:text-left md:text-base lg:w-3/4 mb-8 md:mb-0'>Reunite lost treasures and found items effortlessly – your community's dedicated lost and found platform for quick connections.</p>
                         </Fade>
                     </div>
                     <Fade direction='right' triggerOnce={true}>
-                        <div>
-                            <img className='w-6/12 md:w-8/12 mx-auto rounded-xl' src="https://img.freepik.com/free-vector/detective-kids-concept-illustration_114360-14825.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740" alt="" />
+                        <div className=''>
+                            <motion.img
+                                src="https://img.freepik.com/free-vector/detective-kids-concept-illustration_114360-14825.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740"
+                                animate={{ y: [10, 30, 10] }}
+                                transition={{duration: 3, repeat: Infinity }}
+
+                                className="max-w-[300px] md:h-full h-[200px]  lg:max-w-sm mx-auto rounded-xl shadow-2xl"
+                            />
                         </div>
                     </Fade>
                 </div>
+
+                {/* <img className='w-6/12 md:w-8/12 mx-auto rounded-xl' src="https://img.freepik.com/free-vector/detective-kids-concept-illustration_114360-14825.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740" alt="" /> */}
+
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
@@ -32,7 +42,15 @@ const Slider = () => {
                         <p className='text-base-400 text-sm text-center md:text-left md:text-base mb-8 md:mb-0'>Quickly report lost items by providing key details, increasing the chances of your cherished belongings being returned by finders.</p>
                     </div>
                     <div>
-                        <img className='w-9/12 mx-auto rounded-xl' src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-626.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740" alt="" />
+                        <motion.img
+                            // className='w-9/12 mx-auto rounded-xl'
+                            src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-626.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740"
+                            animate={{ y: [10, 30, 10] }}
+                            transition={{duration: 3, repeat: Infinity }}
+
+                            className="max-w-[300px] md:h-full h-[200px]  lg:max-w-sm mx-auto rounded-xl shadow-2xl"
+                        />
+
                     </div>
                 </div>
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -48,7 +66,14 @@ const Slider = () => {
                         <p className='text-base-400 text-sm text-center md:text-left md:text-base mb-8 md:mb-0'>Explore our Browse Found Items section to quickly view recently submitted belongings and hopefully reunite with your lost possessions.</p>
                     </div>
                     <div>
-                        <img className='w-9/12 md:w-10/12 mx-auto rounded-xl' src="https://img.freepik.com/premium-vector/file-searching-storage-trending-concept-flat-illustration_720185-1553.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740" alt="" />
+                        <motion.img
+                            // className='w-9/12 md:w-10/12 mx-auto rounded-xl' 
+                            src="https://img.freepik.com/premium-vector/file-searching-storage-trending-concept-flat-illustration_720185-1553.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740"
+                            animate={{ y: [10, 30, 10] }}
+                            transition={{duration: 3, repeat: Infinity }}
+
+                            className="max-w-[300px] md:h-full h-[200px]  lg:max-w-sm mx-auto rounded-xl shadow-2xl"
+                        />
                     </div>
                 </div>
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -64,7 +89,14 @@ const Slider = () => {
                         <p className='text-base-400 text-sm text-center md:text-left md:text-base mb-8 md:mb-0'>Engage directly with finders and owners through our platform to securely coordinate the successful recovery of your lost possessions.</p>
                     </div>
                     <div>
-                        <img className='w-9/12 md:w-11/12 mx-auto rounded-xl' src="https://img.freepik.com/premium-vector/man-woman-solar-panel-concept-solar-energy-vector_174639-59449.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740" alt="" />
+                        <motion.img
+                            // className='w-9/12 md:w-11/12 mx-auto rounded-xl' 
+                            src="https://img.freepik.com/premium-vector/man-woman-solar-panel-concept-solar-energy-vector_174639-59449.jpg?ga=GA1.1.1819153903.1738087789&semt=ais_items_boosted&w=740"
+                            animate={{ y: [10, 30, 10] }}
+                            transition={{ duration: 3, repeat: Infinity }}
+
+                            className="max-w-[300px] md:h-full h-[200px]  lg:max-w-sm mx-auto rounded-xl shadow-2xl"
+                        />
                     </div>
                 </div>
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
